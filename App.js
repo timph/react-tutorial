a79f4bbbfe70ff357e3ab9d2e3dd8395d9d6cb64
Tim Pham <tim_ph@yahoo.com>
2016-10-07 19:45:57 -0500

    add all App files as import

--- App.js
+++ App.js
@@ -1,2 +1,6 @@
-import App from './App-composable-comp';
-export default App
+import App1 from './App-states'
+import App2 from './App-comp-high-order'
+import App3 from './App-comp-lifecycle'
+import App4 from './App-composable-comp'
+
+export default App4
